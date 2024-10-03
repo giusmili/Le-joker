c225ce0a17fda09df5e729fd60119cddf995a177
giusmili <giusmili67@gmail.com>
2024-10-03 16:36:30 +0200

    php

--- controller/baseController.php
+++ controller/baseController.php
@@ -51,13 +51,26 @@
             ];
             return $actors;
         }
+
+        # function supp
+        public function storyline(){
+            $story = ["title"=>"Histoire du film", 
+                    "storyline"=>"En 1981, à Gotham City, Arthur Fleck, un comédien raté, sombre dans la folie
+                    après une série de tragédies et d'humiliations. Incapable de se connecter aux
+                    autres et désespéré par l'absence de sens dans sa vie, Arthur se transforme
+                    progressivement en Joker, une figure de chaos et de violence, qui devient 
+                    une icône pour les citoyens les plus marginalisés."];
+            return $story;      
+        }
+       
     }
 
    
 
     # instance
+   
     $newtitle = new User();
   
-    
+   
 
 ?>
