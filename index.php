77b3f769bc18e749e8cb77ed036dc7d5914cd7ef
giusmili <giusmili67@gmail.com>
2024-10-07 11:05:21 +0200

    php

--- index.php
+++ index.php
@@ -27,34 +27,18 @@
         <!-- Section Storyline -->
         <section class="storyline">
         <?php
-            # $actors =  $newtitle->actorsInfo();
              $story = $newtitle->storyline();
-            # print_r($story) 
-         
-            /*  print "<h2>".$story['title']."</h2>\n"; 
-            print "<p>".$story['storyline']."<p>"; */
+      
         ?>
             
-                <h2>
-                    <?= $story['title'] ?>
-                </h2>
+            <h2>
+                <?= $story['title'] ?>
+            </h2>
                 <p>
                     <?= $story['storyline'] ?>
                 </p>
              
-           
         
-             
-            <!-- <h2>Histoire</h2>
-            <p>
-                En 1981, à Gotham City, Arthur Fleck, un comédien raté, sombre dans la folie
-                après une série de tragédies et d'humiliations. Incapable de se connecter aux
-                autres et désespéré par l'absence de sens dans sa vie, Arthur se transforme
-                progressivement en Joker, une figure de chaos et de violence, qui devient 
-                une icône pour les citoyens les plus marginalisés.
-            </p> -->
-           
-         
         </section>
 
         <!-- Section Cast -->
